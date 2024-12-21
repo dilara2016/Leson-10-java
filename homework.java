587f213d074151738be28cc5639525d0ca1484df
dilara2016 <m.123.hunain@gmail.com>
2024-12-21 11:31:08 +0100

    homework

---
+++ homework.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    @Override
+    public String toString() {
+        return "Main []";
+    }
+
+    public static void main(final String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("enter two numbers:");
+            final int x = sc.nextInt();
+            final int y = sc.nextInt();
+
+            final int z = x / y;
+            System.out.println("Result = " + z);
+        } catch (final ArithmeticException ex) {
+            System.out.println("Exception: cannot divide a number by zero!");
+        } catch (final ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Exception: please provide minimum 2 cmd args");
+        } catch (final NumberFormatException ex) {
+            System.out.println("Exception: please provide number without decimal point as args");
+        }
+        System.out.println("--- DONE ---");
+    }
+}
--- main1.java
+++ main1.java
@@ -1,67 +1,38 @@
 import java.util.Scanner;
-public class main1{
-    public class main{
-        public static Candidate getCandidateDetails() throws
-    InvalidInternException {
+
+public class Main {
+    public static Candidate getCandidateDetails() throws InvalidInternException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the candidate details");
+        System.out.println("Enter the candidate Details");
 
         System.out.println("Name");
         String name = scanner.next();
+
         System.out.println("Gender");
         String gender = scanner.next();
-        System.out.println("Enter percentage in 10th");
-        int percentage = scanner.nextInt();
 
-        if (percentage < 50){
-            throw new InvalidInternException("Registration Failed. Percentage cannot be less then 50%");
+        System.out.println("Enter Percentage in 10th");
+        int percentage = scanner.nextInt();
+        if (percentage < 50) {
+            throw new InvalidInternException("Registration Failed. Percentage cannot be less then 50 %.");
         } else {
-            Candidate candidate = new candidate();
-            candidate.setname(name);
+            Candidate candidate = new Candidate();
+            candidate.setName(name);
             candidate.setGender(gender);
             candidate.setPercentage(percentage);
 
             return candidate;
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("Welcome to InterHiring Tool");
 
         try {
             Candidate candidate = getCandidateDetails();
-            System.out.println("Registration Succesful");
+            System.out.println("Registration Successful");
         } catch (InvalidInternException e) {
             System.out.println(e.getMessage());
-
         }
     }
-}
-public class Candidate {
-    private String name;
-    private String gender;
-    private int percentage;
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getGender(){
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public double getPercentage(){
-        return percentage;
-    }
-
-    public void setPercentage(int percentage){
-        this.percentage = percentage;
-    }
-}
 }
